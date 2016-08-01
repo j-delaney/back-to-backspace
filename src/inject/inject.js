0b2eb1280fc73c794aadf517c10c73fe4bc2f68c
Maxim Baz <git@maximbaz.com>
2016-08-01 15:28:57 +0200

    Fix contentEditable #9 #10 #11

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,8 +5,7 @@ const tagBlacklist = [
     'select',
     'option',
     'datalist',
-    'keygen',
-    '[contenteditable]'
+    'keygen'
 ];
 
 const matches = /Chrome\/([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)/.exec(navigator.userAgent);
@@ -27,14 +26,16 @@ if (version.major > 52 ||
         if (event.key === "Backspace" || event.keyCode == 8) {
             // Get the active element.
             // Check to see if the user has focus on a blacklisted element.
-            if ( tagBlacklist.indexOf(document.activeElement.nodeName.toLowerCase()) === -1 ) {
+            if ( tagBlacklist.indexOf(document.activeElement.nodeName.toLowerCase()) === -1
+              && document.activeElement.contentEditable !== "true"
+              && document.activeElement.contentEditable !== "plaintext-only") {
                 // If backspace is pressed on a site like Google Search, Google
                 // will move the focus to the input field. Causing you to see
                 // the last character of the input field to be removed before
                 // going back (or forward) a page.
                 // This disables that behavior.
                 event.stopImmediatePropagation();
-                
+
                 // Go forward in history if the shift key is being held down.
                 // Go backwards in history if the shift key is not being held down.
                 history.go( event.shiftKey ? 1 : -1 );
