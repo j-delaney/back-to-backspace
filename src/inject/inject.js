f783a5a21bbd9e569274d9e66fd313f4ba848212
izy521 <kyekye321@gmail.com>
2016-08-01 04:31:17 +0300

    Clean up code

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -26,30 +26,18 @@ if (version.major > 52 ||
         // Check if key pressed was backspace.
         if (event.key === "Backspace") {
             // Get the active element.
-            let activeEl = document.activeElement;
-            let activateBack = true;
-
             // Check to see if the user has focus on a blacklisted element.
-            tagBlacklist.forEach(function(selector) {
-                if (activeEl.matches(selector))
-                    activateBack = false;
-            });
-
-            if (activateBack) {
+            if ( tagBlacklist.indexOf(document.activeElement.nodeName.toLowerCase()) === -1 ) {
                 // If backspace is pressed on a site like Google Search, Google
                 // will move the focus to the input field. Causing you to see
                 // the last character of the input field to be removed before
                 // going back (or forward) a page.
                 // This disables that behavior.
                 event.stopImmediatePropagation();
-
-                if (event.shiftKey) {
-                    // Go forward in history if the shift key is being held down.
-                    history.go(1);
-                } else {
-                    // Go backwards in history if the shift key is not being held down.
-                    history.go(-1);
-                }
+                
+                // Go forward in history if the shift key is being held down.
+                // Go backwards in history if the shift key is not being held down.
+                history.go( event.shiftKey ? 1 : -1 );
             }
         }
     }, true); // Sites like Google Search move you to the input field when
